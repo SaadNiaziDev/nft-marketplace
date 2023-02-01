567537a7c5ae3608d61b7e5544ebe9ef1c69cf98
SaadNiaziDev <saad.niaxi47@gmail.com>
2023-02-01 20:36:04 +0500

    nfts listed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
-import './App.css';
-import Navbar from './components/Navbar.js';
-import Marketplace from './components/Marketplace';
-import Profile from './components/Profile';
-import SellNFT from './components/SellNFT';
-import NFTPage from './components/NFTpage';
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import Marketplace from "./components/Marketplace";
+import Profile from "./components/Profile";
+import SellNFT from "./components/SellNFT";
+import NFTPage from "./components/NFTpage";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { goerli } from "wagmi/chains";
+import { useAccount, useContract, useSigner } from "wagmi";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./config/constants";
 
 function App() {
-  return (
-    <div className="container">
-        <Routes>
-          <Route path="/" element={<Marketplace />}/>
-          <Route path="/nftPage" element={<NFTPage />}/>        
-          <Route path="/profile" element={<Profile />}/>
-          <Route path="/sellNFT" element={<SellNFT />}/>             
-        </Routes>
-    </div>
-  );
+	const { data: signer } = useSigner({
+		chainId: goerli.id,
+	});
+	const { address } = useAccount();
+	const contract = useContract({
+		address: CONTRACT_ADDRESS,
+		abi: CONTRACT_ABI,
+		signerOrProvider: signer,
+	});
+
+	return (
+		<div className='container'>
+			<Routes>
+				<Route path='/' element={<Marketplace contract={contract} />} />
+				<Route path='/nftPage/:id' element={<NFTPage contract={contract} address={address} />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/sellNFT' element={<SellNFT />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
--- src/components/NFTpage.js
+++ src/components/NFTpage.js
@@ -1,47 +1,78 @@
 import Navbar from "./Navbar";
 import axie from "../tile.jpeg";
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams } from "react-router-dom";
 import MarketplaceJSON from "../Marketplace.json";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { parseEther } from "ethers/lib/utils.js";
 
-export default function NFTPage (props) {
+export default function NFTPage({ contract, address }) {
+	const params = useParams();
 
-const [data, updateData] = useState({});
-const [message, updateMessage] = useState("");
-const [currAddress, updateCurrAddress] = useState("0x");
+	const [data, updateData] = useState({});
+	const [message, updateMessage] = useState("");
 
-    return(
-        <div style={{"min-height":"100vh"}}>
-            <Navbar></Navbar>
-            <div className="flex ml-20 mt-20">
-                <img src={data.image} alt="" className="w-2/5" />
-                <div className="text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5">
-                    <div>
-                        Name: {data.name}
-                    </div>
-                    <div>
-                        Description: {data.description}
-                    </div>
-                    <div>
-                        Price: <span className="">{data.price + " ETH"}</span>
-                    </div>
-                    <div>
-                        Owner: <span className="text-sm">{data.owner}</span>
-                    </div>
-                    <div>
-                        Seller: <span className="text-sm">{data.seller}</span>
-                    </div>
-                    <div>
-                    { currAddress == data.owner || currAddress == data.seller ?
-                        <div className="text-emerald-700">You are the owner of this NFT</div>
-                        : <button className="enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm">Buy this NFT</button>
-                    }
-                    
-                    <div className="text-green text-center mt-3">{message}</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	const getNftData = async (index) => {
+		try {
+			const metadata = await contract.getListedTokenForId(index);
+			updateData(metadata);
+			console.log(metadata);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const buyNFT = async () => {
+		const price = data?.price / 10 ** 18;
+		try {
+			const tx = await contract.executeSale(params.id, { value: parseEther(price.toString()) });
+			await tx.wait();
+			console.log(tx);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		if (contract) {
+			getNftData(params.id);
+		}
+	}, [contract]);
+
+	return (
+		<div style={{ "min-height": "100vh" }}>
+			<Navbar></Navbar>
+			<div className='flex ml-20 mt-20'>
+				<img src={data?.imgURL} alt='' className='w-2/5' />
+				<div className='text-xl ml-20 space-y-8 text-white shadow-2xl rounded-lg border-2 p-5'>
+					<div>Name: {data?.name}</div>
+					<div>Description: {data?.description}</div>
+					<div>
+						Price: <span className=''>{data?.price?.toString() / 10 ** 18 + " ETH"}</span>
+					</div>
+					<div>
+						Owner: <span className='text-sm'>{data?.ownedBy}</span>
+					</div>
+					<div>
+						Seller: <span className='text-sm'>{data?.soldBy}</span>
+					</div>
+					{address && (
+						<div>
+							{address == data?.ownedBy || address == data?.soldBy ? (
+								<div className='text-emerald-700'>You are the owner of this NFT</div>
+							) : (
+								<button
+									className='enableEthereumButton bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-sm'
+									onClick={() => buyNFT()}>
+									Buy this NFT
+								</button>
+							)}
+
+							<div className='text-green text-center mt-3'>{message}</div>
+						</div>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 }
